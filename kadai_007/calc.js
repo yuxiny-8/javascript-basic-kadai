f6e39386348d2b28efcca9a7077311a2db429019
yuxiny-8 <yukaxiny@gmail.com>
2026-07-14 15:19:39 +0900

    条件分岐課題

---
+++ kadai_007/calc.js
@@ -0,0 +1,14 @@
+let num = 10;
+
+if (num % 3 == 0){
+  console.log('3の倍数です');
+}
+else if(num % 5 == 0){
+  console.log('5の倍数です');
+}
+else if(num % 3 && num % 5 == 0){
+  console.log('3と5の倍数です')
+}
+else{
+  console.log(num);
+}
