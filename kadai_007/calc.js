2c1430b4154bf81ec68b7f8ec656c247fe9b56ad
yuxiny-8 <yukaxiny@gmail.com>
2026-07-14 15:33:32 +0900

    Update calc.js

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,13 +1,13 @@
 let num = 10;
 
-if (num % 3 == 0){
+if(num % 3 ==0  && num % 5 == 0){
+  console.log('3と5の倍数です');
+}
+else if (num % 3 == 0){
   console.log('3の倍数です');
 }
 else if(num % 5 == 0){
   console.log('5の倍数です');
-}
-else if(num % 3 && num % 5 == 0){
-  console.log('3と5の倍数です')
 }
 else{
   console.log(num);
